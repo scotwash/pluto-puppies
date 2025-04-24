d3c9e2e53c030ac268a6342cd320a886c7ecdffe
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-04-24 15:17:49 -0400

    added database to index.html

--- js/scotch/home.js
+++ js/scotch/home.js
@@ -8,16 +8,49 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 
-// edit button
-const randomCryptidButton = document.getElementById("random-button-two");
-randomCryptidButton.addEventListener("click", getRandomLinks);
+
 // retrieve data fromm database 
 const db = getFirestore(app);
-const cryptidLinksCollection = collection(db, "pluto-puppies-links");
+const cryptids = collection(db,"pluto-puppies");
 
-async function getRandomLinks() {
-    const randomCryptidLinks = await getDocs(cryptidLinksCollection);
-    var randomWebsiteGen = randomCryptidLinks[Math.floor(Math.random() * randomCryptidLinks.length)];
+// ref members cited as a const
+const cryptidRef = document.querySelector("#members-list");
+// create id for cryptid ref
+cryptidRef.id = "hi";
+// create imgs 
 
+//style cryptidref
+
+document.getElementById("hi").style.paddingLeft = "20px";
+document.getElementById("hi").style.fontFamily = "monospace";
+document.getElementById("hi").style.textAlign= "center";
+
+
+// prints members' cryptids
+async function getCryptidList() {
+    const hasDocs = await getDocs(cryptids);
+    cryptidRef.innerHTML = "";
+
+    for(let i = 0; i < hasDocs.docs.length; i ++)
+    {
+        const currentCryptid = hasDocs.docs[i];
+        console.log(currentCryptid.id);
+        const cryptidData = currentCryptid.data();
+        cryptidRef.innerHTML += "<p>" + "Ada researched the "+ cryptidData.state + " and " + cryptidData.state2 + "!" + "</p>";
+        // add image here 
+        cryptidRef.innerHTML += "<img> " + cryptidData.stateimgone + cryptidData.stateimgtwo + "</img>"
+        cryptidRef.innerHTML += "<br> <p>" + "Scotch researched the " + cryptidData.state3 + " and " + cryptidData.state4 + "!" + "</p";
+        cryptidRef.innerHTML += "<br> <p>" + "Sheryl researched the " + cryptidData.state5 + "!";
+        cryptidRef.innerHTML += "<br> <p>" + "Sunny researched the " + cryptidData.state6 + " and " + cryptidData.state7 + "!" + "</p";
+        
     
+    }
 }
+
+
+
+
+
+
+
+getCryptidList();
