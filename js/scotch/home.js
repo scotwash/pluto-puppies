b3de712abce3be7568f2e1dc63123e3cd56a35e7
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-04-24 11:42:45 -0400

    added button

--- js/scotch/home.js
+++ js/scotch/home.js
@@ -1,8 +1,25 @@
 import { app } from "../app.js";
-console.clear();
+
 import {
     getFirestore,
     collection,
     getDocs,
     addDoc,
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+
+// create a button
+const randomButton = document.createElement("button");
+randomButton.textContent = "Let's visit some cryptids!";
+randomButton.id = "random-button-id";
+randomButton.setAttribute('type','button');
+randomButton.addEventListener("click", function getRandomLinks(){})
+// retrieve data fromm database 
+const db = getFirestore(app);
+const cryptidLinksCollection = collection(db, "pluto-puppies-links");
+
+async function getRandomLinks() {
+    const randomCryptidLinks = await getDocs(cryptidLinksCollection);
+
+    
+}
