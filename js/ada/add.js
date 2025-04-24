a309ef900cec6a2fb9506a0a1a0f322cfbec49a7
adbisesi <adbisesi@iu.edu>
2025-04-24 19:03:49 -0400

    git push

--- js/ada/add.js
+++ js/ada/add.js
@@ -1,47 +1,21 @@
-      // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-      import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_THT-wgU0Pr7eXlUmQfqJKlCJ_b7s3mM",
+  authDomain: "contactplutopuppies.firebaseapp.com",
+  projectId: "contactplutopuppies",
+  storageBucket: "contactplutopuppies.firebasestorage.app",
+  messagingSenderId: "937806378282",
+  appId: "1:937806378282:web:798ddcf089fc00460bda53",
+  measurementId: "G-5XLRJ6GBJK"
+};
 
-      import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
-
-      // TODO: Add SDKs for Firebase products that you want to use
-
-      // https://firebase.google.com/docs/web/setup#available-libraries
-
-      // Your web app's Firebase configuration
-
-      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-      const firebaseConfig = {
-        apiKey: "AIzaSyA_THT-wgU0Pr7eXlUmQfqJKlCJ_b7s3mM",
-
-        authDomain: "contactplutopuppies.firebaseapp.com",
-
-        databaseURL: "https://contactplutopuppies-default-rtdb.firebaseio.com",
-
-        projectId: "contactplutopuppies",
-
-        storageBucket: "contactplutopuppies.firebasestorage.app",
-
-        messagingSenderId: "937806378282",
-
-        appId: "1:937806378282:web:2f63e1c7ad9557010bda53",
-
-        measurementId: "G-RKBKD1ZEN3",
-      };
-
-      // Initialize Firebase
-
-      const app = initializeApp(firebaseConfig);
-
-      const db = getDatabase(app);
-      document.getElementById("subject").addEventListener('click', function(e){
-        set(ref(db, 'user/' + document.getElementById("fname").value), 
-      
-      fname = document.getElementById("fname").value),
-      email = document.getElementById("lname").value,
-      subject = document.getElementById("subject").value,
-      
-    });
-      alert("Message Sent !");
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
