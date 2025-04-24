511d2156b27571d080aa0473ee96f5937e06d1bf
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-04-24 19:34:06 -0400

    began making image gallery

--- js/scotch/home.js
+++ js/scotch/home.js
@@ -9,21 +9,22 @@ import {
 
 
 
+
+
 // retrieve data fromm database 
 const db = getFirestore(app);
 const cryptids = collection(db,"pluto-puppies");
 
 // ref members cited as a const
 const cryptidRef = document.querySelector("#members-list");
+
 // create id for cryptid ref
 cryptidRef.id = "hi";
-// create imgs 
-
 
 
 document.getElementById("hi").style.paddingLeft = "20px";
 document.getElementById("hi").style.fontFamily = "monospace";
-document.getElementById("hi").style.textAlign= "center";
+
 
 
 // prints members' cryptids
@@ -31,14 +32,16 @@ async function getCryptidList() {
     const hasDocs = await getDocs(cryptids);
     cryptidRef.innerHTML = "";
 
+
     for(let i = 0; i < hasDocs.docs.length; i ++)
     {
         const currentCryptid = hasDocs.docs[i];
         console.log(currentCryptid.id);
         const cryptidData = currentCryptid.data();
-        cryptidRef.innerHTML += "<p>" + "Ada researched the "+ cryptidData.state + " and " + cryptidData.state2 + "!" + "</p>";
-        // add image here 
-        cryptidRef.innerHTML += "<br> <p>" + "Scotch researched the " + cryptidData.state3 + " and " + cryptidData.state4 + "!" + "</p";
+        cryptidRef.innerHTML += "<p>" + "Ada researched the "+ cryptidData.state + " and " + cryptidData.state2 + "!" + " These creatures are so cute and fluffy! Maybe the Not-Deer can give some Rudolph some time off for Christmas.</p>";
+        // add image here
+    
+        cryptidRef.innerHTML += "<br> <p>" + "Scotch researched the " + cryptidData.state3 + " and " + cryptidData.state4 + "!" + "";
         cryptidRef.innerHTML += "<br> <p>" + "Sheryl researched the " + cryptidData.state5 + "!";
         cryptidRef.innerHTML += "<br> <p>" + "Sunny researched the " + cryptidData.state6 + " and " + cryptidData.state7 + "!" + "</p";
         
@@ -48,8 +51,5 @@ async function getCryptidList() {
 
 
 
-
-
-
-
 getCryptidList();
+generateRandomGallery();
