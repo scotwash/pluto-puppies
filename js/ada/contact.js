409bf954ef6ecb36003853f503bc85fbbe27fef2
adbisesi <adbisesi@iu.edu>
2025-04-24 18:26:45 -0400

    contact to best of my ability

--- js/ada/contact.js
+++ js/ada/contact.js
@@ -1,1 +1,42 @@
 import { app } from "../add.js";
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskValue = taskInput.value.trim();
+
+    if (taskValue) {
+        taskList.push({ id: Date.now(), task: taskValue });
+        taskInput.value = '';
+        displayTasks();
+    }
+}
+
+// Function to display tasks
+function displayTasks() {
+    const taskListElement = document.getElementById('taskList');
+    taskListElement.innerHTML = '';
+
+    taskList.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = `${task.task} <button onclick="editTask(${task.id})">Edit</button> <button onclick="deleteTask(${task.id})">Delete</button>`;
+        taskListElement.appendChild(li);
+    });
+}
+
+// Function to edit a task
+function editTask(id) {
+    const task = taskList.find(task => task.id === id);
+    const newTaskValue = prompt('Edit task:', task.task);
+
+    if (newTaskValue) {
+        task.task = newTaskValue;
+        displayTasks();
+    }
+}
+
+// Function to delete a task
+function deleteTask(id) {
+    taskList = taskList.filter(task => task.id !== id);
+    displayTasks();
+}
+
