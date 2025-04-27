225bf5b9dcbf6b2338b05d66d6ca8be38e08cb3b
milanwashington54@gmail.com <milanwashington54@gmail.com>
2025-04-26 22:15:52 -0400

    added shared database

--- js/ada/add.js
+++ js/ada/add.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA_THT-wgU0Pr7eXlUmQfqJKlCJ_b7s3mM",
-  authDomain: "contactplutopuppies.firebaseapp.com",
-  projectId: "contactplutopuppies",
-  storageBucket: "contactplutopuppies.firebasestorage.app",
-  messagingSenderId: "937806378282",
-  appId: "1:937806378282:web:798ddcf089fc00460bda53",
-  measurementId: "G-5XLRJ6GBJK"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBN1gqwbTzca8UKPU_n9Kxplquv3W9bHjw",
+    authDomain: "scotwash-final-db.firebaseapp.com",
+    projectId: "scotwash-final-db",
+    storageBucket: "scotwash-final-db.firebasestorage.app",
+    messagingSenderId: "636462454794",
+    appId: "1:636462454794:web:1945c8a0021c513edd27f3",
+    measurementId: "G-LFKWVJ4NNR"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
